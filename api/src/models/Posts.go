692383f224467e6f795de2e2ffae5e429ac69df7
mattsz <6matheuslima@gmail.com>
2025-01-17 14:05:48 -0300

    :bug: fix: wrong struct type

--- api/src/models/Posts.go
+++ api/src/models/Posts.go
@@ -11,38 +11,38 @@ type Post struct {
 	Title      string    `json:"title,omitempty"`
 	Content    string    `json:"content,omitempty"`
 	AuthorID   uint64    `json:"author_id,omitempty"`
-	AuthorNick uint64    `json:"author_nick,omitempty"`
+	AuthorNick string    `json:"author_nick,omitempty"`
 	Likes      uint64    `json:"likes"`
 	CreatedAt  time.Time `json:"created_at,omitempty"`
 }
 
 func (post *Post) Prepare() error {
 	if err := post.validate(); err != nil {
-    return err
-  }
+		return err
+	}
 
-  if err := post.format(); err != nil {
-    return err
-  }
+	if err := post.format(); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (post *Post) validate() error {
 	if post.Title == "" {
-    return errors.New("title must be provided")
-  }
+		return errors.New("title must be provided")
+	}
 
-  if post.Content == "" {
-    return errors.New("content must be provided")
-  }
+	if post.Content == "" {
+		return errors.New("content must be provided")
+	}
 
-  return nil
+	return nil
 }
 
 func (post *Post) format() error {
 	post.Title = strings.TrimSpace(post.Title)
-  post.Content = strings.TrimSpace(post.Content)
+	post.Content = strings.TrimSpace(post.Content)
 
-  return nil
+	return nil
 }
